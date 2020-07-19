40b72c1d851100cda100f0ed804d0ac73e1d33ff
Italo Bravo <ibrav051@uottawa.ca>
2020-07-19 19:53:42 -0400

    'FirstVersion'

---
+++ Assignment5/code/compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Dad\Documents\SEG\Assignment5\Assignment5\code
+java -jar umple.jar Car.ump -c -
+@pause
---
+++ Assignment5/code/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Dad\Documents\SEG\Assignment5\Assignment5\code
+java Car.java
